8ed82c5f5439b30a8ddc3d0ead92c9bd76bf2ba8
root <root@mx>
2024-12-05 00:00:00 +0530

    commit 20241205 #1

---
+++ main.sh
@@ -0,0 +1,39 @@
+git init
+touch junk
+
+declare -i x
+echo "Enter Beginning date [yyyymmdd]"
+read x
+
+declare -i y
+echo "Enter End date [yyyymmdd]"
+read y
+
+DATE=$x
+
+while [ $DATE -le $y ]
+do
+    rand=$(( RANDOM % 100 ))
+
+    if [ $rand -lt 30 ]; then
+        commits=0
+    elif [ $rand -lt 55 ]; then
+        commits=1
+    elif [ $rand -lt 75 ]; then
+        commits=2
+    elif [ $rand -lt 90 ]; then
+        commits=3
+    else
+        commits=4
+    fi
+
+    for ((i=1; i<=commits; i++))
+    do
+        echo "a" >> junk
+        git add .
+        msg="commit ${DATE} #$i"
+        git commit -m "$msg" --date="$(date -R -d ${DATE})"
+    done
+
+    DATE=$(date +%Y%m%d -d "$DATE + 1 day")
+done
